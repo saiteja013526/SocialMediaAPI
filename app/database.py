adcf2a55236e81dce0a8ba3c68998c8fde05b0a6
saiteja <saitejathatikonda0135@gmail.com>
2025-07-13 17:21:26 +0530

    removed raw sql and implemented SQLAlchemy

--- app/database.py
+++ app/database.py
@@ -1,6 +1,7 @@
 import time
 import psycopg2
 from sqlalchemy import create_engine;
+from psycopg2.extras import RealDictCursor;
 from sqlalchemy.ext.declarative import declarative_base;
 from sqlalchemy.orm import sessionmaker 
 
